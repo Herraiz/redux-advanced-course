7e329bf453405f44e63cf76dc3a6de1f84eb5b0a
Rober Herraiz <herraiz92@gmail.com>
2025-01-24 09:39:58 +0000

    feat: adding reducers

--- 1-javascript-redux/script.js
+++ 1-javascript-redux/script.js
@@ -1,10 +1,10 @@
 const ORDER_PIZZA = "ORDER_PIZZA";
 
 // Action
-const action = {
-  type: "ORDER_PIZZA",
-  shopName: "Pizza shop",
-};
+// const action = {
+//   type: "ORDER_PIZZA",
+//   shopName: "Pizza shop",
+// };
 
 // Action creator
 function orderPizza() {
@@ -13,3 +13,22 @@ function orderPizza() {
     shopName: "Pizza shop",
   };
 }
+
+// Reducer
+const initialState = {
+  pizzasBase: 100,
+  toppings: ["cheese", "capsicum"],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ORDER_PIZZA:
+      return {
+        ...state,
+        pizzasBase: state.pizzasBase - 1,
+      };
+
+    default:
+      return state;
+  }
+};
