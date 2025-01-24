1746cc13838e021575a6c7b4f186a152f6285c64
Rober Herraiz <herraiz92@gmail.com>
2025-01-24 10:11:59 +0000

    feat: understanding stores

--- 1-javascript-redux/script.js
+++ 1-javascript-redux/script.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const ORDER_PIZZA = "ORDER_PIZZA";
 
 // Action
@@ -10,14 +13,14 @@ const ORDER_PIZZA = "ORDER_PIZZA";
 function orderPizza() {
   return {
     type: "ORDER_PIZZA",
-    shopName: "Pizza shop",
+    // shopName: "Pizza shop",
   };
 }
 
 // Reducer
 const initialState = {
   pizzasBase: 100,
-  toppings: ["cheese", "capsicum"],
+  //   toppings: ["cheese", "capsicum"],
 };
 
 const reducer = (state = initialState, action) => {
@@ -32,3 +35,21 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// Store
+// 1 - Store needs to hold application state
+
+const store = createStore(reducer);
+
+// 2 - Holds method to acces to the state
+console.log("Initial state:", store.getState());
+
+// 3 -  Register listeners via subscribe(listener)
+store.subscribe(() => {
+  console.log("Updated state:", store.getState());
+});
+
+// 4 - Allow state to be updated via dispatch(action)
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
