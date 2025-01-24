f3433fd07a4df1ad86c4c5df96757d54e0a3aa95
Rober Herraiz <herraiz92@gmail.com>
2025-01-24 09:26:56 +0000

    feat: action and action creator

--- 1-javascript-redux/script.js
+++ 1-javascript-redux/script.js
@@ -1,1 +1,15 @@
-console.log("Welcome to Redux");
+const ORDER_PIZZA = "ORDER_PIZZA";
+
+// Action
+const action = {
+  type: "ORDER_PIZZA",
+  shopName: "Pizza shop",
+};
+
+// Action creator
+function orderPizza() {
+  return {
+    type: "ORDER_PIZZA",
+    shopName: "Pizza shop",
+  };
+}
